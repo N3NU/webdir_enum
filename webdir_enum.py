8e6729b90f4cc5293f6c670cd44f714cc8f04304
N3NU <stevencaraballo27@gmail.com>
2022-01-29 16:04:59 -0500

    Added file_extensions variable

--- webdir_enum.py
+++ webdir_enum.py
@@ -8,15 +8,21 @@ import sys
 subdirectory_list = open("wordlist.txt").read()
 #Assign variable with a list of subdirectory_list content
 directories = subdirectory_list.splitlines()
+#Assign variabe with different file extensions
+file_extensions = ["html","txt","php", \
+                   "xml","py","jsp", \
+                   "js","aspx","dll", \
+                   "shtml","rb","yml"]
 
-#Iterate through the words in directories list
-for dir in directories:
-    #Assign variable with FQDN and directory
-    dir_enum = f"http://{sys.argv[1]}/{dir}.html"
-    #Assign variable with response from server
-    response = requests.get(dir_enum)
-    #Check if response code is 404 (Not Found), pass if True
-    if response.status_code==404:
-        pass
-    else:
-        print("Valid directory:" ,dir_enum)
+for extension in file_extensions:
+    #Iterate through the words in directories list
+    for dir in directories:
+        #Assign variable with FQDN and directory
+        dir_enum = f"http://{sys.argv[1]}/{dir}.{extension}"
+        #Assign variable with response from server
+        response = requests.get(dir_enum)
+        #Check if response code is 404 (Not Found), pass if True
+        if response.status_code==404:
+            pass
+        else:
+            print("Valid directory:" ,dir_enum)
