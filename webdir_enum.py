db5cd9824f803edeea1d2d56640418e2525cedc6
N3NU <stevencaraballo27@gmail.com>
2022-01-30 22:31:32 -0500

    Added ability to search without file extension

--- webdir_enum.py
+++ webdir_enum.py
@@ -9,17 +9,17 @@ subdirectory_list = open("wordlist.txt").read()
 #Assign variable with a list of subdirectory_list content
 directories = subdirectory_list.splitlines()
 #Assign variabe with different file extensions
-file_extensions = ["html","txt","php", \
-                   "xml","py","jsp", \
-                   "js","aspx","dll", \
-                   "shtml","rb","yml"]
+file_extensions = ["",".html",".txt",".php", \
+                   ".xml",".py",".jsp", \
+                   ".js",".aspx",".dll", \
+                   ".shtml",".rb",".yml"]
 
 if len(sys.argv) < 3:
     for extension in file_extensions:
         #Iterate through the words in directories list
         for dir in directories:
             #Assign variable with FQDN and directory
-            dir_enum = f"http://{sys.argv[1]}/{dir}.{extension}"
+            dir_enum = f"http://{sys.argv[1]}/{dir}{extension}"
             #Assign variable with response from server
             response = requests.get(dir_enum)
             #Check if response code is 404 (Not Found), pass if True
